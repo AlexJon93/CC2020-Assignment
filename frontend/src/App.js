925afdb240009a252e1e8537b47233e49434570f
gregoryvalkovic <gregoryvalkovic@gmail.com>
2020-04-25 14:28:39 +1000

    Add registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './js/Login';
 import Main from './js/Main';
+import Register from './js/Register';
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={Main}/>
-      <Route path="/login" component={Login}/>
+      <Route path="/login" exact component={Login}/>
+      <Route path="/register" exact component={Register}/>
     </Router>
   );
 }
--- frontend/src/js/Register.js
+++ frontend/src/js/Register.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
 import '../App.css'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-
-function Login() {
+function Register() {
     return (
         <div>
             <Form className="form-signin center-text align-middle">
@@ -14,11 +13,15 @@ function Login() {
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Label>Email address</Form.Label>
                                 <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
                             </Form.Group>
 
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="passwordConfirm" placeholder="Password (confirm)" />
                             </Form.Group>
                             <Button variant="primary" type="submit">
                                 Submit
@@ -26,11 +29,11 @@ function Login() {
                     </Card.Body>
                 </Card>
                 <Form.Text className="text-muted">
-                    <a href="/register">Register a new account</a><br/>
+                    <a href="/login">Already have an account? Login</a><br/>
                 </Form.Text>
             </Form>
-        </div>
+        </div> 
     );
 }
 
-export default Login;
+export default Register;
