5e5a1e0a80db9b41bfec5169186bfb98294ada35
gregoryvalkovic <gregoryvalkovic@gmail.com>
2020-04-25 13:44:05 +1000

    Login page

---
+++ frontend/src/js/Login.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../App.css'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+function Login() {
+    return (
+        <div>
+            <Form className="form-signin center-text align-middle">
+                <Card className="form-card" >
+                    <Card.Body>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                    </Card.Body>
+                </Card>
+                <Form.Text className="text-muted">
+                    <a href="/register">Register a new account</a><br/>
+                </Form.Text>
+            </Form>
+        </div>
+    );
+}
+
+export default Login;
